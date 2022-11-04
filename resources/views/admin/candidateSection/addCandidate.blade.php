60cdb917ad264365cb8ca737c94ac722e8bb6571
Wajahat <h.wajahat.tv@gmail.com>
2022-11-04 23:29:46 +0500

    add candidate page improved

--- resources/views/admin/candidateSection/addCandidate.blade.php
+++ resources/views/admin/candidateSection/addCandidate.blade.php
@@ -82,13 +82,7 @@
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-md-4">
-                                                <div class="form-group">
-                                                    <label for="exampleInputEmail1">Age</label>
-                                                    <input type="number" class="form-control addInput" name="age"
-                                                        id="" aria-describedby="" placeholder="" maxlength="13">
-                                                </div>
-                                            </div>
+
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label for="exampleInputEmail1">Occupation</label>
@@ -115,6 +109,54 @@
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <div class="form-group">
+                                                        <label for="exampleInputEmail1">Religion</label>
+                                                        <select class="form-select" name="religion" id="selectOccupation">
+                                                            <option selected disabled>--select--</option>
+                                                            <option value="" selected="selected" disabled="disabled">-- select one --</option>
+                                                            <option value="African Traditional &amp; Diasporic">African Traditional &amp; Diasporic</option>
+                                                            <option value="Agnostic">Agnostic</option>
+                                                            <option value="Atheist">Atheist</option>
+                                                            <option value="Baha'i">Baha'i</option>
+                                                            <option value="Buddhism">Buddhism</option>
+                                                            <option value="Cao Dai">Cao Dai</option>
+                                                            <option value="Chinese traditional religion">Chinese traditional religion</option>
+                                                            <option value="Christianity">Christianity</option>
+                                                            <option value="Hinduism">Hinduism</option>
+                                                            <option value="Islam">Islam</option>
+                                                            <option value="Jainism">Jainism</option>
+                                                            <option value="Juche">Juche</option>
+                                                            <option value="Judaism">Judaism</option>
+                                                            <option value="Neo-Paganism">Neo-Paganism</option>
+                                                            <option value="Nonreligious">Nonreligious</option>
+                                                            <option value="Rastafarianism">Rastafarianism</option>
+                                                            <option value="Secular">Secular</option>
+                                                            <option value="Shinto">Shinto</option>
+                                                            <option value="Sikhism">Sikhism</option>
+                                                            <option value="Spiritism">Spiritism</option>
+                                                            <option value="Tenrikyo">Tenrikyo</option>
+                                                            <option value="Unitarian-Universalism">Unitarian-Universalism</option>
+                                                            <option value="Zoroastrianism">Zoroastrianism</option>
+                                                            <option value="primal-indigenous">primal-indigenous</option>
+                                                            <option value="Other">Other</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <div class="form-group">
+                                                        <label for="exampleInputEmail1">Local Church</label>
+                                                        <input type="text" class="form-control addInput" name="local_church"
+                                                               id="" aria-describedby="" placeholder="">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <hr/>
                                         <div class="form-group">
                                             <label for="exampleInputEmail1">Candidate's Mobile number</label>
                                             <input type="number" class="form-control addInput" name="phone_no"
