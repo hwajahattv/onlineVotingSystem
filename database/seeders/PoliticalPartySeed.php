ff991fe5defe2b2362215119f09fe1fb4da9d3fc
Wajahat <h.wajahat.tv@gmail.com>
2022-11-14 14:50:41 +0500

    Home page improved

---
+++ database/seeders/PoliticalPartySeed.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PoliticalParty;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PoliticalPartySeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        PoliticalParty::create(
+            [
+            'name' => 'Allegiance Party (ALG)'
+            ]);
+        PoliticalParty::create(
+            [
+            'name' => 'Christian Democratic Party (CDP)'
+            ]);
+        PoliticalParty::create(
+            [
+            'name' => 'Coalition for Reform Party (CFRP)'
+            ]);
+        PoliticalParty::create(
+            [
+            'name' => 'Grassroot United Front Party (GRUF)'
+            ]);
+        PoliticalParty::create(
+            [
+            'name' => 'Mapai Levites Party (MLP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Melanesian Alliance Party (MAP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Melanesian Liberal Party (MLP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Model Nation Party (PKP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'National Alliance Party (NA)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'National Conservative Party (NCP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Nations` Interest Party (NIP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'New Generation Party (NGP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Our Development Party (ODP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Papua and Niugini Union Party (PANGU Party)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Papua New Guinea Party (PNGP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Paradise Kingdom Party (PKP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Action Party (PAP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Democratic Movement (PDM)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s First Party (PFP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Freedom Party (UP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Labour Party (PLP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Movement for Change Party (PMFCP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s National Congress (PNC)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Party (PP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Progress Party (PPP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s Resource Awareness Party (PRAP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'People`s United Assembly Party (PUAP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG Country Party (PNCP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG Destiny Party (PNGDP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG First Party (PNGFP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG Greens Party (PNGGP)PNG Human Rights Party (PNGHRP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG National Party (PNGNP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG One Nation Party (PONP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG Socialist Party (PNGSP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'PNG Youths Party (PNGYP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Republican Party (RP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Social Democratic Party (SDP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'SOM Pioneer Party (SOM)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Star Alliance Party (SAP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Triumph Heritage Empowerment Party (THEP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Trust PNG Party (TPNGP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'United Labour Party (PTU)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'United Party (UP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'United Resources Party (URP)'
+            ]);
+        PoliticalParty::create(
+            [
+                'name' => 'Wantok in Godly Services Party(WINGS)'
+            ]
+        );
+    }
+}
