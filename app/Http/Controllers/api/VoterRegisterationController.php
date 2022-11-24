edf03609521c1d7e92025f2eb9cbab56e8930531
Wajahat <h.wajahat.tv@gmail.com>
2022-11-24 11:15:20 +0500

    test link created for aamer

---
+++ app/Http/Controllers/api/VoterRegisterationController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Voter;
+use Illuminate\Http\Request;
+
+class VoterRegisterationController extends Controller
+{
+    //
+    public function registerAsVoterPostTest(Request $request){
+        {
+
+            $data = $request->all();
+
+            //model call
+
+            $voter = new Voter;
+
+            $voter->name = $data["name"];
+
+
+            //image validation
+            $status=$voter->save();
+            $message ="";
+
+            if($status==false){
+                return [
+                    "status" => 0,
+                    "$message" =>"Voter not created"
+                ];
+            }
+            else
+                return [
+                    "status" => 1,
+                    "$message" =>"Voter created"
+                ];
+        }
+
+    }
+
+}
