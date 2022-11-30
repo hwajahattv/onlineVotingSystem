3f1f3cadd8f67af5aa50557506750e1f771e272c
Wajahat <h.wajahat.tv@gmail.com>
2022-11-30 15:52:30 +0500

    mapping shifted to DB

--- app/Http/Controllers/api/VoterRegisterationController.php
+++ app/Http/Controllers/api/VoterRegisterationController.php
@@ -19,7 +19,24 @@ class VoterRegisterationController extends Controller
             $voter = new Voter;
 
             $voter->name = $data["name"];
-            $voter->birth_region = $data["birth_region"];
+            $voter->middleName = $data["middleName"];
+            $voter->surName = $data["surName"];
+            $voter->age = $data["age"];
+            $voter->dob = $data["surName"];
+            $voter->occupation = $data["occupation"];
+            if($data['occupation']=="School"){
+                $voter->school = $data["school"];
+            }
+            $voter->religion = $data["religion"];
+            if($data["religion"] == "others"){
+                $voter->otherReligion = $data["otherReligion"];
+            }
+            $voter->local_church = $data["local_church"];
+            $voter->current_region = $data["current_region"];
+            $voter->current_province = $data["current_province"];
+            $voter->current_district = $data["current_district"];
+            $voter->current_LLG = $data["current_LLG"];
+            $voter->current_ward = $data["current_ward"];
 
 
             //image validation
