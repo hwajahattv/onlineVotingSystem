23903628d6408d156b9d1026be8bc23b0cb76589
Wajahat <h.wajahat.tv@gmail.com>
2022-12-01 19:12:00 +0500

    api candidate registration

--- public/js/generateVoteCastURL.js
+++ public/js/generateVoteCastURL.js
@@ -2,8 +2,8 @@ function generateVoteCastURL(){
     var btn=document.getElementById("openBallotBtn");
     var inputField=document.getElementById("inputID");
     inputID=inputField.value;
-    if(inputID!=""){
-        document.getElementById("openBallotBtn").href = '/castVote/'+inputID;
+    if(inputID!==""){
+        document.getElementById("openBallotBtn").href = 'url("/castVote/'+inputID+'")';
     }
     else{
         document.getElementById("openBallotBtn").href = 'javascript:void(0)';
