4602248e7f36d367101e7672f10e68e0dcef0dbc
Wajahat <h.wajahat.tv@gmail.com>
2022-12-01 20:11:17 +0500

    api cast vote

--- app/Http/Controllers/api/VoteCastAPIController.php
+++ app/Http/Controllers/api/VoteCastAPIController.php
@@ -20,24 +20,29 @@ class VoteCastAPIController extends Controller
                 "message" => 'Voter ID not Found! Find your Voter ID or get registered as voter first.'
             ];
         } else {
-//        dd($voter);
-//        $allCandidates=Candidate::all();
-            $region = $voter->current_region;
-            $province = $voter->current_province;
-            $district = $voter->current_district;
-            $LLG = $voter->current_LLG;
-            $ward = $voter->current_ward;
-            $election = Election::all();
-            $candidates = Candidate::where(['current_region' => $region])->where(['current_province' => $province])->where(['current_district' => $district])->where(['current_LLG' => $LLG])->where(['current_ward' => $ward])->with('politicalParty')->get();
+            if ($voter->current_region == null || $voter->current_province == null || $voter->current_district == null || $voter->current_LLG = null || $voter->current_ward == null) {
+                return [
+                    "status" => 0,
+                    "message" => 'Voter data not filled completely.'
+                ];
+            } else {
+                $region = $voter->current_region;
+                $province = $voter->current_province;
+                $district = $voter->current_district;
+                $LLG = $voter->current_LLG;
+                $ward = $voter->current_ward;
+                $election = Election::all();
+                $candidates = Candidate::where(['current_region' => $region])->where(['current_province' => $province])->where(['current_district' => $district])->where(['current_LLG' => $LLG])->where(['current_ward' => $ward])->with('politicalParty')->get();
 //    dd($candidates,$allCandidates, $voter,$region, $province, $district, $LLG, $ward);
 
-            return [
-                "status" => 1,
-                "message" => 'Voter data found',
-                'voter' => $voter,
-                'election' => $election,
-                'candidates' => $candidates,
-            ];
+                return [
+                    "status" => 1,
+                    "message" => 'Voter data found',
+                    'voter' => $voter,
+                    'election' => $election,
+                    'candidates' => $candidates,
+                ];
+            }
         }
     }
 }
