c866d1b9ccbc830823352dc3292e241c9c65fe2d
Wajahat <h.wajahat.tv@gmail.com>
2022-12-01 11:25:10 +0500

    api candidate registration

--- app/Http/Controllers/api/VoterLookUpController.php
+++ app/Http/Controllers/api/VoterLookUpController.php
@@ -15,11 +15,11 @@ class VoterLookUpController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:25',
             'surName' => 'required|max:25',
-            'birth_region' => 'required',
-            'birth_province' => 'required',
-            'birth_district' => 'required',
-//            'birth_LLG' => 'required',
-//            'birth_ward' => 'required',
+            'current_region' => 'required',
+            'current_province' => 'required',
+            'current_district' => 'required',
+//            'current_LLG' => 'required',
+//            'current_ward' => 'required',
         ]);
         if ($validator->fails()) {
             $message ="";
@@ -32,9 +32,9 @@ class VoterLookUpController extends Controller
         $requiredVoter=Voter::
         where(['name'=>$request['name']])
             ->where(['surName'=>$request['surName']])
-            ->where(['birth_region'=>$request['birth_region']])
-            ->where(['birth_province'=>$request['birth_province']])
-            ->where(['birth_district'=>$request['birth_district']])->first();
+            ->where(['current_region'=>$request['current_region']])
+            ->where(['current_province'=>$request['current_province']])
+            ->where(['current_district'=>$request['current_district']])->first();
         if($requiredVoter==null){
             return [
                 "status" => 0,
